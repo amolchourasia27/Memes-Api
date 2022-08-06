c99efba86058784e5d3dacb56e54d9f64142b68e
amolchourasia27 <amolchourasia27@outlook.com>
2022-08-06 14:28:02 +0530

    heroku5

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 const posts = require("./posts");
-app.listen(process.env.PORT || 5000, () => console.log("Server running"));
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
 app.get("/posts", (req, res) => {
   const data = Math.floor(Math.random() * 299);
