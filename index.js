7ad2fcb0d31ee80e453467cf7ef2d09791bb3b57
amolchourasia27 <amolchourasia27@outlook.com>
2022-08-06 14:29:25 +0530

    heroku6

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const posts = require("./posts");
+// const port = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
